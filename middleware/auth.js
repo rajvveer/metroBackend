a0c1cca26aa6dc43dc0fdefca682b370c9750731
Rajveer(Chiranjeev)Shekhawat <121601167+rajvveer@users.noreply.github.com>
2025-03-03 14:38:09 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,18 +4,24 @@ const jwt = require("jsonwebtoken");
 const User = require("../model/user");
 const Shop = require("../model/shop");
 
-exports.isAuthenticated = catchAsyncErrors(async(req,res,next) => {
-    const {token} = req.cookies;
-
-    if(!token){
-        return next(new ErrorHandler("Please login to continue", 401));
-    }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
-    req.user = await User.findById(decoded.id);
-
-    next();
+exports.isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  // Try to get token from cookies first, then from authorization header
+  const token = req.cookies.token || req.headers.authorization;
+  
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Please login to continue"
+    });
+  }
+  
+  // Verify the token using the secret key
+  const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  
+  // Fetch the user based on the token's decoded id
+  req.user = await User.findById(decoded.id);
+  
+  next();
 });
 
 
